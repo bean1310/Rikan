598b6fbcf6d8782d2bed8f40b20cdcd3ba14f9ac
bean1310 <bean@beanlog.xyz>
2022-02-05 00:08:09 +0900

    getMemoryMapまで書いた
    正常に動く保証はない

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,55 @@ use utf16_literal::utf16;
 
 mod uefi;
 
+struct MemoryMap<'a> {
+    buffer_size: usize,
+    buffer: &'a mut [u8],
+    map_size: usize,
+    map_key: usize,
+    descriptor_size: usize,
+    descriptor_version: u32,
+}
+
+fn getMemoryMap(memoryMap: &mut MemoryMap, gBS: &uefi::EfiBootServices) -> uefi::EfiStatus {
+    memoryMap.map_size = memoryMap.buffer_size;
+    gBS.GetMemoryMap(
+        &mut memoryMap.map_size,
+        &mut memoryMap.buffer,
+        &mut memoryMap.map_key,
+        &mut memoryMap.descriptor_size,
+        &mut memoryMap.descriptor_version,
+    )
+}
+
 #[no_mangle]
-pub extern "C" fn efi_main(ImageHandle: uefi::EFI_HANDLE, SystemTable: &uefi::SystemTable) -> uefi::EFI_STATUS {
+pub extern "C" fn efi_main(
+    ImageHandle: uefi::EfiHandle,
+    SystemTable: &uefi::SystemTable,
+) -> uefi::EfiStatus {
     let _conout = SystemTable.ConOut();
     _conout.Reset(false);
-    _conout.OutputString(utf16!("Hello World\r\n").as_ptr());
+    _conout.OutputString(utf16!("Hello World\n").as_ptr());
 
-    loop{}
+    let mut buffer: [u8; 4096 * 4] = [0; 4096 * 4];
+    let mut memoryMap = MemoryMap {
+        buffer_size: 4096 * 4,
+        buffer: &mut buffer,
+        map_size: 0,
+        map_key: 0,
+        descriptor_size: 0,
+        descriptor_version: 0,
+    };
 
-    uefi::EFI_STATUS::Success
+    getMemoryMap(&mut memoryMap, SystemTable.BootServices());
+
+    _conout.OutputString(utf16!("hoge").as_ptr());
+
+    loop {}
+
+    uefi::EfiStatus::Success
 }
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop{}
+    loop {}
 }
--- src/uefi.rs
+++ src/uefi.rs
@@ -1,70 +1,129 @@
-use core::ffi::c_void;
+use core::{ffi::c_void};
 
 #[repr(C)]
-pub enum EFI_STATUS {
-    Success = 0
+pub enum EfiStatus {
+    Success = 0,
 }
 
-type CHAR16 = u16;
+type Char16 = u16;
 
 #[repr(C)]
-pub struct EFI_TABLE_HEADER{
-    Signature:  u64,
-    Revision:   u32,
+pub struct EfiTableHeader {
+    Signature: u64,
+    Revision: u32,
     HeaderSize: u32,
-    CRC32:      u32,
-    Reserved:   u32
+    Crc32: u32,
+    Reserved: u32,
 }
 
-pub struct EFI_HANDLE(*mut c_void);
+pub struct EfiHandle(*mut c_void);
 
-pub struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    Reset: extern "efiapi" fn(This: &EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL, ExtendedVerification: bool) -> EFI_STATUS,
-    OutputString: extern "efiapi" fn(This: &EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL, String: *const CHAR16) -> EFI_STATUS,
-    _Unuse0: u64,
-    _Unuse1: u64,
-    _Unuse2: u64,
-    _Unuse3: u64,
-    _Unuse4: u64,
-    _Unuse5: u64,
-    _Unuse6: u64,
-    _Unuse7: u64
+pub struct EfiSimpleTextOutputProtocol {
+    Reset: extern "efiapi" fn(
+        This: &EfiSimpleTextOutputProtocol,
+        ExtendedVerification: bool,
+    ) -> EfiStatus,
+    OutputString: extern "efiapi" fn(
+        This: &EfiSimpleTextOutputProtocol,
+        String: *const Char16,
+    ) -> EfiStatus,
+    _Unuse0: usize,
+    _Unuse1: usize,
+    _Unuse2: usize,
+    _Unuse3: usize,
+    _Unuse4: usize,
+    _Unuse5: usize,
+    _Unuse6: usize,
+    _Unuse7: usize,
 }
 
-impl EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    pub fn Reset(&self, ExtendedVerification: bool) -> EFI_STATUS {
-        unsafe {(self.Reset)(self, ExtendedVerification)}
+impl EfiSimpleTextOutputProtocol {
+    pub fn Reset(&self, ExtendedVerification: bool) -> EfiStatus {
+        unsafe { (self.Reset)(self, ExtendedVerification) }
     }
 
-    pub fn OutputString(&self, String: *const CHAR16) -> EFI_STATUS {
-        unsafe{(self.OutputString)(self, String)}
+    pub fn OutputString(&self, String: *const Char16) -> EfiStatus {
+        unsafe { (self.OutputString)(self, String) }
     }
 }
 
-pub struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL{}
-pub struct EFI_RUNTIME_SERVICES{}
-pub struct EFI_BOOT_SERVICES{}
-pub struct EFI_CONFIGURATION_TABLE{}
+pub struct EfiSimpleTextInputProtocol {}
+pub struct EfiRuntimeService {}
+pub struct EfiBootServices {
+    Hdr: EfiTableHeader,
+    RaiseTPL: usize,
+    RestoreTPL: usize,
+    AllocatePages: usize,
+    FreePages: usize,
+    GetMemoryMap: extern "efiapi" fn(
+        MemoryMapSize: *mut usize,
+        MemoryMap: *mut [EfiMemoryDescriptor],
+        MapKey: *mut usize,
+        DescriptorSize: *mut usize,
+        DescriptoraVersion: *mut u32,
+    ) -> EfiStatus,
+    AllocatePool: usize,
+    FreePool: usize,
+}
+
+impl EfiBootServices {
+    pub fn GetMemoryMap(
+        &self,
+        MemoryMapSize: &mut usize,
+        MemoryMap: &mut [u8],
+        MapKey: &mut usize,
+        DescriptorSize: &mut usize,
+        DescriptoraVersion: &mut u32,
+    ) -> EfiStatus {
+        unsafe {
+            (self.GetMemoryMap)(
+                MemoryMapSize as *mut usize,
+                // MemoryMap as *mut [EfiMemoryDescriptor],
+                (MemoryMap as *mut [u8]) as *mut [EfiMemoryDescriptor],
+                MapKey as *mut usize,
+                DescriptorSize as *mut usize,
+                DescriptoraVersion as *mut u32,
+            )
+        }
+    }
+}
+
+#[repr(C)]
+pub struct EfiMemoryDescriptor {
+    Type: u32,
+    PhysicalStart: EfiPhysicalAddress,
+    VirtualStart: EfiVirtualAddress,
+    NumberOfPages: u64,
+    Attribute: u64,
+}
+
+pub type EfiPhysicalAddress = u64;
+pub type EfiVirtualAddress = u64;
+pub struct EfiConfigurationTable {}
 
 #[repr(C)]
 pub struct SystemTable {
-    Hdr:                    EFI_TABLE_HEADER,
-    FirmwareVendor:         *const CHAR16,
-    FirmwareRevision:       u32,
-    ConsoleInHandle:        EFI_HANDLE,
-    ConIn:                  *mut EFI_SIMPLE_TEXT_INPUT_PROTOCOL,
-    ConsoleOutHandle:       EFI_HANDLE,
-    ConOut:                 *mut EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL,
-    StandardErrorHandle:    EFI_HANDLE,
-    StdErr:                 *mut EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL,
-    RuntimeServices:        *mut EFI_RUNTIME_SERVICES,
-    BootServices:           *mut EFI_BOOT_SERVICES,
-    NumberOfTableEntries:   usize,
-    EConfigurationTable:    *mut EFI_CONFIGURATION_TABLE
+    Hdr: EfiTableHeader,
+    FirmwareVendor: *const Char16,
+    FirmwareRevision: u32,
+    ConsoleInHandle: EfiHandle,
+    ConIn: *mut EfiSimpleTextInputProtocol,
+    ConsoleOutHandle: EfiHandle,
+    ConOut: *mut EfiSimpleTextOutputProtocol,
+    StandardErrorHandle: EfiHandle,
+    StdErr: *mut EfiSimpleTextOutputProtocol,
+    RuntimeServices: *mut EfiRuntimeService,
+    BootServices: *mut EfiBootServices,
+    NumberOfTableEntries: usize,
+    EConfigurationTable: *mut EfiConfigurationTable,
 }
 
 impl SystemTable {
-    pub fn ConOut(&self) -> &mut EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-        unsafe {&mut *self.ConOut}
+    pub fn ConOut(&self) -> &mut EfiSimpleTextOutputProtocol {
+        unsafe { &mut *self.ConOut }
+    }
+
+    pub fn BootServices(&self) -> &EfiBootServices {
+        unsafe { &*self.BootServices }
     }
 }
